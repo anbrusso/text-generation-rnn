36dc85fc1d2adb9d3295be60de4db701fc02bd02
anbrusso <albrusso@mtu.edu>
2022-03-15 17:22:35 -0400

    Update settings.py

--- textgeneration/textgeneration/settings.py
+++ textgeneration/textgeneration/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*v$fd(-9hmzxz(+*0v&o_c0__!s@@aiuq4g!j0k&^42!*!wdg7'
+SECRET_KEY = os.environ.get("SECRET_KEY",default='*v$fd(-9hmzxz(+*0v&o_c0__!s@@aiuq4g!j0k&^42!*!wdg7')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(",")
 
 
 # Application definition
